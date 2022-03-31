150004eaad5be00835d5edb2efeb9b45438b1014
mrzhamg <2637375274@qq.com>
2022-03-31 17:43:42 +0800

    1.4.4

--- V1.4.2/MiYou-store/src/views/store/storeMsg.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div>消息中心</div>
-</template>
--- V1.4.2/demo1/src/main/java/com/test001/demo/controller/StoreLoginController.java
+++
@@ -1,9 +0,0 @@
-package com.test001.demo.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class StoreLoginController {
-
-
-}
